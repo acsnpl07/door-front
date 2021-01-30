081ef97c61921a75730819003dfdaf6921f16f89
acsnpl07 <Acsnpl07@gmail.com>
2021-01-30 10:04:03 +0200

    changed photo quality

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -41,7 +41,7 @@ export class ProfilePage {
   getImages() {
     this.options = {
       maximumImagesCount: 1,
-      width: 160,
+      width: 200,
       quality: 100,
       outputType: 1,
     };
